0dc681981bb47e40714e6527c483f9408989b8a2
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-11-04 15:18:11 +0800

    dashbaord responsive

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -17,12 +17,12 @@
                     </div>
                 </header>
                 {{-- * Uppersection --}}
-                <div class="flex gap-4 mt-6">
+                <div class="flex flex-col 2xl:flex-row gap-4 mt-6">
                     {{-- * Attendacne --}}
-                    <div class="bg-white rounded-sm py-12 w-[35%] flex justify-center items-center">
+                    <div class="bg-white rounded-sm py-12 px-4 w-full 2xl:w-[35%] flex justify-center items-center">
                         <div class="flex flex-col items-center gap-8">
                             <p class="text-primary text-xl font-bold">Today's Attendance</p>
-                            <div class="flex justify-between items-center gap-12">
+                            <div class="flex flex-col md:flex-row justify-evenly items-center gap-12 w-full ">
                                 <div class="flex flex-col items-center">
                                     <p class="text-3xl text-text font-bold">24</p>
                                     <p class="text-xs text-text/50 font-semibold">Present</p>
@@ -36,16 +36,16 @@
                                     <p class="text-xs text-text/50 font-semibold">Unknown</p>
                                 </div>
                             </div>
-                            <div class="flex gap-3">
+                            <div class="flex flex-col 2xl:flex-row gap-3">
                                 <button class="mybutton-secondary rounded-dmd">View Attendance</button>
                                 <button class="mybutton-primary rounded-dmd flex justify-center gap-2"><span class="ic-plus text-xl"></span>Add Attendance</button>
                             </div>
                         </div>
                     </div>
                     {{-- * Chart --}}
-                    <div class="bg-white rounded-sm p-8 w-[65%]">
-                        <div class="flex justify-between">
-                            <p class="text-primary text-xl font-bold">Attendance Frequency</p>
+                    <div class="bg-white rounded-sm p-8 w-full 2xl:w-[65%]">
+                        <div class="flex flex-col md:flex-row justify-between items-center">
+                            <p class="text-primary text-lg md:text-xl font-bold">Attendance Frequency</p>
                             <select id="timeframeSelect" class="select bg-transparent focus:outline-none focus:border-none font-bold">
                                 <option value="month" selected>This Month</option>
                                 <option value="week">This Week</option>
@@ -56,93 +56,94 @@
                         </div>
                     </div>
                 </div>
-                <div class="mt-7 p-8 bg-white rounded-sm">
-                    <div class="flex justify-between">
-                        <input type="text" class="bg-[#F1F1F5] rounded-[12px] w-96 py-3 px-4 placeholder:text-xxs font-medium focus:outline-none" placeholder="Search Name">
+                <div class="mt-7 p-8 bg-white rounded-sm ">
+                    <div class="flex flex-col md:flex-row justify-between gap-4">
+                        <input type="text" class="bg-[#F1F1F5] rounded-[12px] w-full md:w-96 py-3 px-4 placeholder:text-xxs font-medium focus:outline-none" placeholder="Search Name">
                         <button class="mybutton-primary rounded-dmd flex justify-center items-center gap-3"onclick="student_add.showModal()"><span class="ic-plus text-base"></span> Add Student</button>
                     </div>
-                    <table class="mt-8 w-full">
-                        <thead class="rounded-dmd">
-                            <tr class="bg-primary/10  text-left">
-                                <th class="p-4 font-semibold text-gray-600">Number</th>
-                                <th class="p-4 font-semibold text-gray-600">Student Name</th>
-                                <th class="p-4 font-semibold text-gray-600">Date Joined</th>
-                                <th class="p-4 font-semibold text-gray-600">Points</th>
-                                <th class="p-4 font-semibold text-gray-600">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody> {{-- ? Tabel Data --}}
-
-                            <tr class="border-b">
-                                <td class="p-4 text-text">1</td>
-                                <td class="p-4 text-text">Alice Smith</td>
-                                <td class="p-4 text-text">2022-10-16</td>
-                                <td class="p-4 text-text">100</td>
-                                <td class="p-4 flex space-x-2">
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-eye text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-edit text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
-                                        <div class="solar-delete text-xl"></div>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr class="border-b">
-                                <td class="p-4 text-text">2</td>
-                                <td class="p-4 text-text">Dione Stark</td>
-                                <td class="p-4 text-text">2022-10-16</td>
-                                <td class="p-4 text-text">100</td>
-                                <td class="p-4 flex space-x-2">
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-eye text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-edit text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
-                                        <div class="solar-delete text-xl"></div>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr class="border-b">
-                                <td class="p-4 text-text">3</td>
-                                <td class="p-4 text-text">Ayam Kambing</td>
-                                <td class="p-4 text-text">2022-10-16</td>
-                                <td class="p-4 text-text">100</td>
-                                <td class="p-4 flex space-x-2">
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-eye text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-edit text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
-                                        <div class="solar-delete text-xl"></div>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr class="">
-                                <td class="p-4 text-text">4</td>
-                                <td class="p-4 text-text">Alice Smith</td>
-                                <td class="p-4 text-text">2022-10-16</td>
-                                <td class="p-4 text-text">100</td>
-                                <td class="p-4 flex space-x-2">
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-eye text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
-                                        <div class="solar-edit text-xl"></div>
-                                    </button>
-                                    <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
-                                        <div class="solar-delete text-xl"></div>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div class="overflow-x-scroll">
+                        <table class="mt-8 w-full">
+                            <thead class="rounded-dmd">
+                                <tr class="bg-primary/10  text-left">
+                                    <th class="p-4 font-semibold text-text">Number</th>
+                                    <th class="p-4 font-semibold text-text">Student Name</th>
+                                    <th class="p-4 font-semibold text-text">Date Joined</th>
+                                    <th class="p-4 font-semibold text-text">Points</th>
+                                    <th class="p-4 font-semibold text-text">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody> {{-- ? Tabel Data --}}
+                                <tr class="border-b">
+                                    <td class="p-4 text-text">1</td>
+                                    <td class="p-4 text-text">Alice Smith</td>
+                                    <td class="p-4 text-text">2022-10-16</td>
+                                    <td class="p-4 text-text">100</td>
+                                    <td class="p-4 flex space-x-2">
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-eye text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-edit text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
+                                            <div class="solar-delete text-xl"></div>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr class="border-b">
+                                    <td class="p-4 text-text">2</td>
+                                    <td class="p-4 text-text">Dione Stark</td>
+                                    <td class="p-4 text-text">2022-10-16</td>
+                                    <td class="p-4 text-text">100</td>
+                                    <td class="p-4 flex space-x-2">
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-eye text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-edit text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
+                                            <div class="solar-delete text-xl"></div>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr class="border-b">
+                                    <td class="p-4 text-text">3</td>
+                                    <td class="p-4 text-text">Ayam Kambing</td>
+                                    <td class="p-4 text-text">2022-10-16</td>
+                                    <td class="p-4 text-text">100</td>
+                                    <td class="p-4 flex space-x-2">
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-eye text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-edit text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
+                                            <div class="solar-delete text-xl"></div>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr class="">
+                                    <td class="p-4 text-text">4</td>
+                                    <td class="p-4 text-text">Alice Smith</td>
+                                    <td class="p-4 text-text">2022-10-16</td>
+                                    <td class="p-4 text-text">100</td>
+                                    <td class="p-4 flex space-x-2">
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-eye text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-blue-100 text-blue-600 hover:bg-blue-200 flex justify-center items-center">
+                                            <div class="solar-edit text-xl"></div>
+                                        </button>
+                                        <button class="p-2 rounded bg-red-100 text-red-600 hover:bg-red-200 flex justify-center items-center">
+                                            <div class="solar-delete text-xl"></div>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
